6ea8c78f2644b1abe716456ea135702c9f51b0b5
PlayerG9 <g9yout@gmail.com>
2022-05-10 10:28:04 +0200

    improved install-script

--- install.sh
+++ install.sh
@@ -10,6 +10,10 @@ python3 -m venv .venv || exit 1
 .venv/bin/pip install --upgrade pip
 .venv/bin/pip install -r ./requirements.txt
 
+echo
+echo "If you want this command to available from everywhere, then add the following line to ~/.bash_aliases"
+echo "alias yt='$(realpath src/yt/run.sh)'"
+
 # simple output for the user
 echo
 echo "Project is ready to go"
