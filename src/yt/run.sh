f56ffe79f50bb2ed82154df375f880cbf024812e
PlayerG9 <g9yout@gmail.com>
2022-05-10 10:02:33 +0200

    added a script to run the command independent of the location

---
+++ src/yt/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# execute the package with the .venv python interpreter
+# just python3 would use the global python that doesn't have the required packages installed
+"$(dirname "$0")/../../.venv/bin/python3" "$(dirname "$0")/../yt" "$@"
+
