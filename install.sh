62ad72550a97b7b5e0c2995724325905a9f467ce
PlayerG9 <g9yout@gmail.com>
2022-05-24 10:00:48 +0200

    fixed some typos and improved the script

--- install.sh
+++ install.sh
@@ -10,6 +10,9 @@ python3 -m venv .venv || exit 1
 .venv/bin/pip install --upgrade pip
 .venv/bin/pip install -r ./requirements.txt
 
+echo
+echo "Virtual environment was created/updated"
+echo
 
 # add alias command (create if not exists)
 ALIAS_COMMAND="alias yt='$(realpath src/yt/run.sh)'"
@@ -18,7 +21,7 @@ ALIAS_FILE="$HOME/.bash_aliases"
 if [[ -f "$ALIAS_FILE" ]]; then
   # check if the command already exists in the file
   if grep -Fxq "$ALIAS_COMMAND" "$ALIAS_FILE"; then
-    echo "alias already exists"
+    echo "'yt'-alias already exists"
   else
     echo "$ALIAS_COMMAND" >> "$ALIAS_FILE"
   fi
@@ -30,22 +33,25 @@ if [[ -f "$ALIAS_FILE" ]]; then
 #    echo "$ALIAS_COMMAND" >> "$BASHRC_FILE"
 #  fi
 else
-  echo "Failed to create alias"
+  echo "Failed to create 'yt'-alias"
 fi
 
 # add bash completion (gets always replaced)
 COMPLETION_DIRECTORY="$HOME/.bash_completion.d/"
 if [[ ! -d "$COMPLETION_DIRECTORY" ]]; then
+  echo "Created '$COMPLETION_DIRECTORY' because it didn't exists"
   mkdir "$COMPLETION_DIRECTORY"
 fi
+
 COMPLETION_FILE="$COMPLETION_DIRECTORY/yt-completion.bash"
 : > "$COMPLETION_FILE" # empty or create the file
 echo '#!/usr/bin/env bash
 complete -W "--help download search" yt
 ' >> "$COMPLETION_FILE"
-echo "Restart terminal for update yt command completion"
+echo "Restart terminal to update yt command completion"
 
 RUN_COMPLETION_COMMAND="source \"$COMPLETION_FILE\""
+"$RUN_COMPLETION_COMMAND"  # run once to update
 if ! grep -Fxq "$RUN_COMPLETION_COMMAND" "$BASHRC_FILE"; then
   echo "$RUN_COMPLETION_COMMAND" >> "$BASHRC_FILE"
 fi
