065d022d1305ee1e58a257f8eb04136cf0f8eef0
PlayerG9 <g9yout@gmail.com>
2022-05-24 09:55:30 +0200

    improved installation script

--- install.sh
+++ install.sh
@@ -10,11 +10,47 @@ python3 -m venv .venv || exit 1
 .venv/bin/pip install --upgrade pip
 .venv/bin/pip install -r ./requirements.txt
 
-echo
-echo "If you want this command to available from everywhere, then add the following line to ~/.bash_aliases"
-echo "alias yt='$(realpath src/yt/run.sh)'"
 
-# simple output for the user
+# add alias command (create if not exists)
+ALIAS_COMMAND="alias yt='$(realpath src/yt/run.sh)'"
+BASHRC_FILE="$HOME/.bashrc"
+ALIAS_FILE="$HOME/.bash_aliases"
+if [[ -f "$ALIAS_FILE" ]]; then
+  # check if the command already exists in the file
+  if grep -Fxq "$ALIAS_COMMAND" "$ALIAS_FILE"; then
+    echo "alias already exists"
+  else
+    echo "$ALIAS_COMMAND" >> "$ALIAS_FILE"
+  fi
+#elif [[ -f "$BASHRC_FILE" ]]; then
+#  # check if the command already exists in the file
+#  if grep -Fxq "$ALIAS_COMMAND" "$BASHRC_FILE"; then
+#    echo "alias already exists"
+#  else
+#    echo "$ALIAS_COMMAND" >> "$BASHRC_FILE"
+#  fi
+else
+  echo "Failed to create alias"
+fi
+
+# add bash completion (gets always replaced)
+COMPLETION_DIRECTORY="$HOME/.bash_completion.d/"
+if [[ ! -d "$COMPLETION_DIRECTORY" ]]; then
+  mkdir "$COMPLETION_DIRECTORY"
+fi
+COMPLETION_FILE="$COMPLETION_DIRECTORY/yt-completion.bash"
+: > "$COMPLETION_FILE" # empty or create the file
+echo '#!/usr/bin/env bash
+complete -W "--help download search" yt
+' >> "$COMPLETION_FILE"
+echo "Restart terminal for update yt command completion"
+
+RUN_COMPLETION_COMMAND="source \"$COMPLETION_FILE\""
+if ! grep -Fxq "$RUN_COMPLETION_COMMAND" "$BASHRC_FILE"; then
+  echo "$RUN_COMPLETION_COMMAND" >> "$BASHRC_FILE"
+fi
+
+# simple output for the user as confirmation
 echo
 echo "Project is ready to go"
 echo
