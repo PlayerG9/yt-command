112ef0a790070e1c9690c550b20ad95b7dce4d82
PlayerG9 <g9yout@gmail.com>
2022-06-28 11:14:11 +0200

    implemented first version of the 'yt search' command

--- src/yt/search/__init__.py
+++ src/yt/search/__init__.py
@@ -2,10 +2,14 @@
 r"""
 search for videos
 
+yt search "Last one standing"
+
 only lists videos between 1min and 6min.
 if you need longer or shorter videos, then use the --longer or --shorter arguments
 """
 import youtubesearchpython
+import logging
+import shutil
 
 
 COMMAND_NAME = "search"
@@ -23,19 +27,59 @@ def initialise(helper: 'argparse.ArgumentParser', commands: dict):  # noqa
 
     parser.add_argument('query')
 
-    parser.add_argument('-l', '--longer', help="removes upper timelimit of filter")
-    parser.add_argument('-s', '--shorter', help="removes lower timelimit of filter")
+    # parser.add_argument('-l', '--longer', help="removes upper timelimit of filter")
+    # parser.add_argument('-s', '--shorter', help="removes lower timelimit of filter")
+
+    parser.add_argument('-c', '--channel', help="specifies a channel-name")
+    parser.add_argument('-L', '--limit', help="limit of results", default=6)
 
     commands[COMMAND_NAME] = execute
 
 
 def execute(arguments):
-    Searcher(query=arguments.query).execute()
+    Searcher(query=arguments.query, config=arguments).execute()
 
 
 class Searcher:
-    def __init__(self, query: str):
+    search: youtubesearchpython.CustomSearch
+
+    def __init__(self, query: str, config):
         self.query = query
+        self.config = config
 
     def execute(self):
-        pass
+        self.createSearcher()
+        self.printSearchResults()
+
+    def createSearcher(self):
+        from youtubesearchpython import CustomSearch, SearchMode, VideoDurationFilter, VideoSortOrder
+        self.search = CustomSearch(
+            query=self.query,
+            searchPreferences=SearchMode.videos + VideoDurationFilter.short + VideoSortOrder.relevance,
+            limit=self.config.limit,
+            timeout=30
+        )
+
+    def printSearchResults(self):
+        result = self.search.result()
+        terminal_width = shutil.get_terminal_size()[0]
+
+        for video in result['result']:
+            video: dict
+            try:
+                title: str = video['title']
+                link: str = video['link']
+                channel_name: str = video['channel']['name']
+                duration: str = video['duration']
+                view_count: str = video['viewCount']['short']  # alternative option is 'text'
+            except KeyError as error:
+                logging.warning("failed to evaluate one result element")
+                logging.warning(f"({error.__class__.__name__}: {error})")
+                continue
+            else:
+                print(f" {title} ".center(terminal_width, "="))
+                print(f"Link:     {link}")
+                print(f"Channel:  {channel_name}")
+                print(f"Duration: {duration}")
+                print(f"Views:    {view_count}")
+                print()  # for a better separation
